3c9f0e9c0360e3396feccb7ffcf3a2790cf44916
khamick <mick2000ita@gmail.com>
2026-02-10 17:02:50 +0100

    Adding details to login page

--- frontend/src/app/login-page/login-page.ts
+++ frontend/src/app/login-page/login-page.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
-import { TestButtonComponent } from "../test-button/test-button.component";
 import { GoogleLoginComponent } from "../google-login-component/google-login-component";
 import { CardModule } from 'primeng/card';
 import { FloatLabelModule } from 'primeng/floatlabel';
+import { PasswordModule } from 'primeng/password';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login-page',
-  imports: [TestButtonComponent, GoogleLoginComponent, CardModule, FloatLabelModule],
+  imports: [ GoogleLoginComponent, CardModule, FloatLabelModule, PasswordModule, FormsModule, InputTextModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
 export class LoginPage {
 
-  user: boolean = true;
 
-   value: string | undefined;
+
+  user: string | undefined;
+  password: string | undefined;
+
 }
