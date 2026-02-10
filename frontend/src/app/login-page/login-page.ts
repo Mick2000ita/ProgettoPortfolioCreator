91c63aa612dc4cd9827e6fef6c98cc5c5e42ea71
khamick <mick2000ita@gmail.com>
2026-02-10 15:46:35 +0100

    Aggiunta modifiche minori

--- frontend/src/app/login-page/login-page.ts
+++ frontend/src/app/login-page/login-page.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { TestButtonComponent } from "../test-button/test-button.component";
 import { GoogleLoginComponent } from "../google-login-component/google-login-component";
 import { CardModule } from 'primeng/card';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-login-page',
-  imports: [TestButtonComponent, GoogleLoginComponent, CardModule],
+  imports: [TestButtonComponent, GoogleLoginComponent, CardModule, FloatLabelModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
@@ -13,4 +14,5 @@ export class LoginPage {
 
   user: boolean = true;
 
+   value: string | undefined;
 }
